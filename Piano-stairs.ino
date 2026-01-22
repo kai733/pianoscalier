9528174ecfc36107854b03b763444d26778596d8
kai733 <kai.djepaxhia@gmail.com>
2026-01-22 09:40:02 +0100

    commentaires

--- Piano-stairs.ino
+++ Piano-stairs.ino
@@ -1,13 +1,13 @@
-#include "Arduino.h"
+#include "Arduino.h" // Import des bibliothèques
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 
-const int LDR_PINS[] = { A0, A1, A2, A3 };
-const int NB_STEPS = 4;
+const int LDR_PINS[] = { A0, A1, A2, A3 }; // Définit les broches des capteurs
+const int NB_STEPS = 4; // Nombre de marches
 
-int trackMapping[] = { 1, 2, 3, 4 };
-bool isPlaying[] = { false, false, false, false };
-int threshold = 800;
+int trackMapping[] = { 1, 2, 3, 4 }; // Associe chaque capteur à un numéro de piste MP3
+bool isPlaying[] = { false, false, false, false }; // Un booléen permet de savoir si un son est déjà en cours de déclenchement.
+int threshold = 800; // Seuil de luminosité: Si les photorésistances passent en dessous de cette valeur cela veut dire que le laser est interrompu
 
 SoftwareSerial mySoftwareSerial(10, 11);
 DFRobotDFPlayerMini myDFPlayer;
@@ -19,33 +19,33 @@ void setup() {
     while (true)
       ;
   }
-  myDFPlayer.volume(30);
+  myDFPlayer.volume(30); // Volume du son (Max à 30)
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    String data = Serial.readStringUntil('\n');
-    if (data.startsWith("M")) {
-      int stepIdx = data.substring(1, 2).toInt() - 1;
-      int trackNum = data.substring(3).toInt();
-      if (stepIdx >= 0 && stepIdx < NB_STEPS) {
-        trackMapping[stepIdx] = trackNum;
+    String data = Serial.readStringUntil('\n'); // Lit la ligne envoyée
+    if (data.startsWith("M")) { // Si ça commence par "M" (ex: M1:05)
+      int stepIdx = data.substring(1, 2).toInt() - 1; // Récupère l'index (0 à 3)
+      int trackNum = data.substring(3).toInt(); // Récupère le numéro de piste
+      if (stepIdx >= 0 && stepIdx < NB_STEPS) { 
+        trackMapping[stepIdx] = trackNum; // Met à jour le tableau
       }
     }
   }
     for (int i = 0; i < NB_STEPS; i++) {
-    int val = analogRead(LDR_PINS[i]);
-    if (val <= threshold) {
-      if (!isPlaying[i]) {
-        myDFPlayer.play(trackMapping[i]);
-        isPlaying[i] = true;
+    int val = analogRead(LDR_PINS[i]); 
+    if (val <= threshold) {  // Si un pied est détecté
+      if (!isPlaying[i]) {  // ET qu'on n'était pas déjà en train de jouer
+        myDFPlayer.play(trackMapping[i]); // --> On joue le son UNE SEULE FOIS
+        isPlaying[i] = true;  // --> On verrouille l'état
         Serial.println("STEP_ON:" + String(i + 1));
       }
     } else {
       if (isPlaying[i]) {
-        isPlaying[i] = false;
+        isPlaying[i] = false; // --> On déverrouille quand le pied part
       }
     }
   }
-  delay(50);
+  delay(50); // --> Petit délai
 }
